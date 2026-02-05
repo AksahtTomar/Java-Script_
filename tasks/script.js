aae41e9aecc1c48e680d9a6f48cd57e19c58848b
Akshat Tomar <akshattomar@1909gmail.com>
2026-02-05 09:21:08 +0530

    Js session

---
+++ tasks/script.js
@@ -0,0 +1,17 @@
+// task01
+// const profileSection = document.getElementById("profile");
+  
+// profileSection.style.backgroundColor = "blue";
+// profileSection.style.padding = "10px";
+//  profileSection.style.textAlign = "center";
+
+
+// task02
+// const importantParagraphs = document.getElementsByClassName("important");
+// console.log(importantParagraphs);
+// for (let i = 0; i < importantParagraphs.length; i++) {
+//     importantParagraphs[i].style.color = "red";
+//     importantParagraphs[i].style.fontWeight = "bold";
+//     importantParagraphs[i].style.fontSize = "18px";
+// }   
+
